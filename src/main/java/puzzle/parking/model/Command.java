ed5fc8425d881d63e01f0a3dcd6e6dc7a7cfe629
nehabk <nehabkshirsagar@gmail.com>
2018-05-25 01:01:31 +0530

    Implemented all the commands

--- src/main/java/puzzle/parking/model/Command.java
+++ src/main/java/puzzle/parking/model/Command.java
@@ -2,6 +2,9 @@ package puzzle.parking.model;
 
 import puzzle.parking.ParkingLot;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static java.lang.String.format;
 
 public enum Command {
@@ -13,6 +16,63 @@ public enum Command {
             parkingLot = new ParkingLot(numberOfParkingSpaces);
 
             System.out.println(format("Created a parking lot with %s slots", numberOfParkingSpaces));
+        }
+    },
+
+    PARK("^park (.*) (.*)") {
+        public void execute(String[] args) {
+            String registrationNumber = args[1];
+            String color = args[2];
+            Car car = new Car(registrationNumber, color);
+            Integer slot = parkingLot.park(car);
+
+            if (slot > 0)
+                System.out.println(format("Allocated slot number: %s", slot));
+            else
+                System.out.println("Sorry, parking lot is full");
+        }
+    },
+
+    LEAVE("^leave ([0-9]+)") {
+        public void execute(String[] args) {
+            Integer slotToVacate = Integer.parseInt(args[1]);
+            parkingLot.leave(slotToVacate);
+
+            System.out.println(format("Slot number %s is free", slotToVacate));
+        }
+    },
+
+    STATUS("^status") {
+        public void execute(String[] args) {
+            System.out.println(parkingLot.getStatus());
+        }
+    },
+
+    REGISTRATION_NUMBERS_FOR_CARS_WITH_COLOR("^registration_numbers_for_cars_with_colour (.*)") {
+        public void execute(String[] args) {
+            String registrationNumber = args[1];
+            String[] registrationNumbers = parkingLot.getRegistrationNumbersForColor(registrationNumber);
+            System.out.println(String.join(", ", registrationNumbers));
+        }
+    },
+
+    SLOT_NUMBERS_FOR_CARS_WITH_COLOR("^slot_numbers_for_cars_with_colour (.*)") {
+        public void execute(String[] args) {
+            String registrationNumber = args[1];
+            int[] slotNumbers = parkingLot.getSlotNumbersForColor(registrationNumber);
+
+            String slotNumbersString = IntStream.of(slotNumbers)
+                    .mapToObj(Integer::toString)
+                    .collect(Collectors.joining(", "));
+
+            System.out.println(slotNumbersString);
+        }
+    },
+
+    SLOT_NUMBER_FOR_REGISTRATION_NUMBER("^slot_number_for_registration_number (.*)") {
+        public void execute(String[] args) {
+            String registrationNumber = args[1];
+            System.out.println(parkingLot.getSlotNumberForRegistrationNumber(registrationNumber));
         }
     };
 
--- src/test/java/puzzle/parking/model/CommandTest.java
+++ src/test/java/puzzle/parking/model/CommandTest.java
@@ -9,7 +9,7 @@ import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static puzzle.parking.model.Command.CREATE_PARKING_LOT;
+import static puzzle.parking.model.Command.*;
 
 public class CommandTest {
 
@@ -25,6 +25,10 @@ public class CommandTest {
         System.setOut(System.out);
     }
 
+    public void resetOutContent() {
+        outContent.reset();
+    }
+
     @Test
     public void should_execute_create_parking_lot_command() {
         String[] args = {"create_parking_lot", "6"};
@@ -33,5 +37,93 @@ public class CommandTest {
 
         assertThat(outContent.toString(), is(output));
     }
+
+    @Test
+    public void should_execute_park_command() {
+        String[] createParkingLotArgs = {"create_parking_lot", "6"};
+        String[] parkArgs = {"park", "KA-01-HH-1234", "White"};
+        String output = "Allocated slot number: 1\n";
+
+        CREATE_PARKING_LOT.execute(createParkingLotArgs);
+        resetOutContent();
+        PARK.execute(parkArgs);
+
+        assertThat(outContent.toString(), is(output));
+    }
+
+    @Test
+    public void should_execute_leave_command() {
+        String[] createParkingLotArgs = {"create_parking_lot", "6"};
+        String[] leaveArgs = {"leave", "4"};
+        String output = "Slot number 4 is free\n";
+
+        CREATE_PARKING_LOT.execute(createParkingLotArgs);
+        resetOutContent();
+        LEAVE.execute(leaveArgs);
+
+        assertThat(outContent.toString(), is(output));
+    }
+
+    @Test
+    public void should_execute_status_command() {
+        String[] createParkingLotArgs = {"create_parking_lot", "6"};
+        String[] parkArgs = {"park", "KA-01-HH-1234", "White"};
+        String[] statusArgs = {"status"};
+        String output = "Slot No.\tRegistration No\tColor\n1\tKA-01-HH-1234\tWhite\n";
+
+        CREATE_PARKING_LOT.execute(createParkingLotArgs);
+        PARK.execute(parkArgs);
+        resetOutContent();
+        STATUS.execute(statusArgs);
+
+        assertThat(outContent.toString(), is(output));
+    }
+
+    @Test
+    public void should_execute_registration_numbers_for_cars_with_colors_command() {
+        String[] createParkingLotArgs = {"create_parking_lot", "6"};
+        String[] parkArgs = {"park", "KA-01-HH-1234", "White"};
+        String[] registrationNumberForColorArgs = {"registration_numbers_for_cars_with_colour", "White"};
+        String output = "KA-01-HH-1234\n";
+
+        CREATE_PARKING_LOT.execute(createParkingLotArgs);
+        PARK.execute(parkArgs);
+        resetOutContent();
+        REGISTRATION_NUMBERS_FOR_CARS_WITH_COLOR.execute(registrationNumberForColorArgs);
+
+        assertThat(outContent.toString(), is(output));
+    }
+
+    @Test
+    public void should_execute_slot_numbers_for_cars_with_colors_command() {
+        String[] createParkingLotArgs = {"create_parking_lot", "6"};
+        String[] parkArgs = {"park", "KA-01-HH-1234", "White"};
+        String[] slotNumberForColorArgs = {"slot_numbers_for_cars_with_colour", "White"};
+        String output = "1, 2\n";
+
+        CREATE_PARKING_LOT.execute(createParkingLotArgs);
+        PARK.execute(parkArgs);
+        PARK.execute(parkArgs);
+        resetOutContent();
+        SLOT_NUMBERS_FOR_CARS_WITH_COLOR.execute(slotNumberForColorArgs);
+
+        assertThat(outContent.toString(), is(output));
+    }
+
+    @Test
+    public void should_execute_slot_number_for_registration_number_command() {
+        String[] createParkingLotArgs = {"create_parking_lot", "6"};
+        String[] parkArgs = {"park", "KA-01-HH-1234", "White"};
+        String[] slotNumberForRegistrationNumberArgs = {"slot_number_for_registration_number", "KA-01-HH-1234"};
+        String output = "1\n";
+
+        CREATE_PARKING_LOT.execute(createParkingLotArgs);
+        PARK.execute(parkArgs);
+        resetOutContent();
+        SLOT_NUMBER_FOR_REGISTRATION_NUMBER.execute(slotNumberForRegistrationNumberArgs);
+
+        assertThat(outContent.toString(), is(output));
+    }
+
 
 }
